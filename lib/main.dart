cb04686ff9a19569472736060e1d0cd4b51908f0
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-21 15:20:35 +0300

    data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,1 @@
-import 'package:catalog_provider/model/prodcut.dart';
-
-void main(List<String> args) {
-  Product p1 = Product(1, "book", 50);
-  Product p2 = Product(2, "note", 100);
-}
+void main(List<String> args) {}
--- lib/model/prodcut.dart
+++ lib/model/prodcut.dart
@@ -5,18 +5,12 @@ class Product {
   String? name;
   Color? color;
   int price = 42;
-
   Product.fromJson(Map<String, dynamic> data) {
     id = data["id"];
     name = data['name'];
     price = data["price"];
     color = Colors.primaries[id! % Colors.primaries.length];
   }
-  Product(this.id, this.name, this.price)
-      // To make the sample app look nicer, each Product is given one of the
-      // Material Design primary colors.
-      : color = Colors.primaries[id! % Colors.primaries.length];
-
   @override
   int get hashCode => id!;
 
