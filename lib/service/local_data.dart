ce93ed0fcf262473eb89c32fd6314916f2880dba
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-21 15:20:30 +0300

    json

---
+++ lib/service/local_data.dart
@@ -0,0 +1,14 @@
+class ConstantData {
+  static List<Map<String, dynamic>> dataAsJson = [
+    {"id": 1, "name": "Code Smell", "price": 40},
+    {"id": 2, "name": "Control Flow", "price": 50},
+    {"id": 3, "name": "Automata", "price": 30},
+    {"id": 4, "name": "Bit Shift", "price": 45},
+    {"id": 5, "name": "Hydra Code", "price": 900},
+    {"id": 6, "name": "Spaghetti", "price": 100},
+    {"id": 7, "name": "Heisenbug", "price": 80},
+    {"id": 8, "name": "Sprint", "price": 55},
+    {"id": 9, "name": "Recursion", "price": 200},
+    {"id": 10, "name": "Bottato", "price": 100},
+  ];
+}
