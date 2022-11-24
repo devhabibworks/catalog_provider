bfbc171ec477aad93d2ab6666a9a0607c885dc91
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-24 14:48:11 +0300

    cart_provider

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -1,1 +1,22 @@
+import 'package:catalog_provider/model/prodcut.dart';
+import 'package:flutter/material.dart';
 
+class CartProvider with ChangeNotifier {
+  final List<Product> _cartProducts = [];
+
+  List<Product> get cartProducts => _cartProducts;
+
+  addProductToCart(Product product) {
+    if (!_cartProducts.contains(product)) {
+      _cartProducts.add(product);
+      notifyListeners();
+    }
+  }
+
+  removeProductFromCart(Product product) {
+    if (_cartProducts.contains(product)) {
+      _cartProducts.remove(product);
+      notifyListeners();
+    }
+  }
+}
